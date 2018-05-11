b7d96d277b7f20988de430246eaf03abb16ab33b
Joe Guo <joeg@catalyst.net.nz>
2018-05-11 15:51:33 +1200

    symbolic constants

--- 1-5-fahrenheit2celsius-reverse-for.c
+++ 1-5-fahrenheit2celsius-reverse-for.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 int main() {
 	int fahrenheit;
 
 	printf("  F      C\n");
-	for (fahrenheit=0; fahrenheit<=300; fahrenheit+=20) {
+	for (fahrenheit=LOWER; fahrenheit<=UPPER; fahrenheit+=STEP) {
 		printf("%3d %6.1f\n", fahrenheit, 5.0*(fahrenheit-32.0)/9.0);
 	}
 }
