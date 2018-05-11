94c8903e6505b3ff7e2ac2fa4970ef2f26e25e30
Joe Guo <joeg@catalyst.net.nz>
2018-05-11 15:15:09 +1200

    change int to float

--- 02-fahrenheit2celsius.c
+++ 02-fahrenheit2celsius.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-	int fahrenheit, celsius;
-	int lower, upper, step;
+	float fahrenheit, celsius;
+	float lower, upper, step;
 
 	lower = 0;
 	upper = 300;
@@ -10,8 +10,8 @@ int main() {
 
 	fahrenheit = lower;
 	while (fahrenheit <= upper) {
-		celsius = 5 * (fahrenheit - 32) / 9;
-		printf("%3d %6d\n", fahrenheit, celsius);
+		celsius = 5.0 * (fahrenheit - 32.0) / 9.0;
+		printf("%3.0f %6.1f\n", fahrenheit, celsius);
 		fahrenheit += step;
 	}
 }
